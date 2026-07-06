83cffe88d3587bf53160b9aa8513395fb8949e20
Charan Sai <guttulacharansai@gmail.com>
2026-07-06 23:17:28 +0530

    Add Nodemon and dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const app = express();
-const PORT  = 5000;
 
 app.get('/',(req,res) => {
     res.send("Sentinel API is running");
 });
 
-app.listen(PORT,() => {
-    console.log(`Listening to port ${PORT}`);
+app.listen(process.env.PORT,() => {
+    console.log(`Listening to port ${process.env.PORT}`);
 });
