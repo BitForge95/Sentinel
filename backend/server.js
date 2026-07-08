f7bf6a4658568f0735b2775b7bb5171a625a98e5
Charan Sai <guttulacharansai@gmail.com>
2026-07-08 23:42:25 +0530

    Add CORS and JSON Middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 const express = require('express')
 const app = express();
+var cors = require('cors');
+
+app.use(cors);
 
 app.get('/',(req,res) => {
     res.send("Sentinel API is running");
@@ -9,3 +12,8 @@ app.get('/',(req,res) => {
 app.listen(process.env.PORT,() => {
     console.log(`Listening to port ${process.env.PORT}`);
 });
+
+app.post('/test',(req,res) => {
+    console.log(req.body);
+    res.send("Data Recieved");
+})
