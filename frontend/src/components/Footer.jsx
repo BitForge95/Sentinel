a199095298bdfc88b88f354d622f15ed64190ae1
Charan Sai <guttulacharansai@gmail.com>
2026-07-09 01:16:16 +0530

    Configure Tailwind and setup basic structure for frontend

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer>
+            <h2>© 2026 Sentinel: Fraud Detection Engine</h2>
+        </footer>
+    )
+}
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <nav>
+            <h1 className="text-4xl font-italic text-red-700">Sentinel Engine</h1>
+        </nav>
+    )
+}
