0461420e8b7ccc43b0c06d08fc4167e6c5f5018b
Charan Sai <guttulacharansai@gmail.com>
2026-07-13 23:42:47 +0530

    Save Mock Transaction to DB

--- backend/controllers/transactionController.js
+++ backend/controllers/transactionController.js
@@ -1,5 +1,20 @@
-function generateMockTransaction(req,res) {
-    res.status(200).json({ message: "Mock transaction endpoint"});
+const Transaction = require('../models/Transaction');
+
+const generateMockTransaction = async (req,res) => {
+    try {
+        const dummy = new Transaction({
+            senderAccount : '111',
+            receiverAccount : '222',
+            amount : 123,
+            currency : 'INR'
+        })
+
+        await dummy.save();
+
+        res.status(201).json(dummy);
+    } catch (error) {
+        res.status(500).json({error : error.message});
+    }
 }
 
 module.exports = {generateMockTransaction};
