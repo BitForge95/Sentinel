fd2207595632657673210acc56fc3ec588822840
Charan Sai <guttulacharansai@gmail.com>
2026-07-26 22:07:55 +0530

    Create Admin Middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -27,4 +27,12 @@ async function Protect(req,res,next) {
     }
 }
 
-module.exports = {Protect};
+const AdminOnly = async (req,res,next) => {
+    if(req.user && req.user.role == 'Admin') {
+        next();
+    } else{
+        res.status(403).json({ error: "Access denied: Admins only" });
+    }
+}
+
+module.exports = {Protect,AdminOnly};
--- backend/routes/fraudRoute.js
+++ backend/routes/fraudRoute.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const {flagTransaction,getFraudLogs,resolveIncident} = require('../controllers/fraudController');
-const {Protect} = require('../middleware/authMiddleware');
+const {Protect,AdminOnly} = require('../middleware/authMiddleware');
 
 router.post('/:id',Protect,flagTransaction);
 router.get('/',Protect,getFraudLogs);
-router.delete('/:id/resolve',Protect ,resolveIncident);
+router.delete('/:id/resolve',Protect,AdminOnly ,resolveIncident);
 
 module.exports = router;
