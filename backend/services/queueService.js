27d680f458aa14aa07c4988697c7f5e883e288aa
Charan Sai <guttulacharansai@gmail.com>
2026-07-30 21:29:01 +0530

    implement Docker containerization for full-stack deployment

--- backend/services/queueService.js
+++ backend/services/queueService.js
@@ -4,8 +4,8 @@ const FraudLog = require('../models/FraudLog');
 const {Queue,Worker} = require('bullmq');
 
 const connection = {
-    host : "127.0.0.1",
-    port : 6380,
+    host : process.env.REDIS_HOST ||  "127.0.0.1",
+    port : process.env.REDIS_PORT || 6380,
 };
 
 const transactionQueue = new Queue("transaction-ingestion", {
