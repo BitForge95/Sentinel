bf168ac064f608b7e32f040f14bc18ca0ebb8f15
Guttula Charan Sai <guttulacharansai@gmail.com>
2026-09-07 10:56:28 +0530

    Remove unused DashboardMock import
    Removed unused import for DashboardMock component.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import Navbar from './components/Navbar';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import DashboardMock from './pages/Dashboardmock';
 
 
 const App = () => {
